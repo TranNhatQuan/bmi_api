8148c52fa131fcef80f50c7520ed4a97875c6fe6
Tran Quan <trannhatquan.2001@gmail.com>
2023-04-10 20:07:05 +0700

    cap nhat them getInfoRecipe

--- routers/recipe.routers.js
+++ routers/recipe.routers.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const {Recipe} = require("../models")
-const {authenticate} = require("../middlewares/auth/authenticate.js")
-const {authorize} = require("../middlewares/auth/authorize.js")
 
-const {getFavorite, getInfoRecipe,getAllRecipe, editRecipe} = require("../controllers/recipe.controllers");
+// const {authenticate} = require("../middlewares/auth/authenticate.js")
+// const {authorize} = require("../middlewares/auth/authorize.js")
+
+const {getInfoRecipe} = require("../controllers/recipe.controllers");
 
 const recipeRouter = express.Router();
 //lay day du thong tin mot mon an
-recipeRouter.get("/:idRecipe", authenticate, getInfoRecipe)
+recipeRouter.get("/:idRecipe", getInfoRecipe)
 //lay cac Recipe ma user co isLike = 1
-recipeRouter.get("/favorite", authenticate, getFavorite) 
-recipeRouter.get("/",authenticate, getAllRecipe)
-recipeRouter.put("/edit",authenticate, authorize([1]), editRecipe)
+// recipeRouter.get("/favorite", authenticate, getFavorite) 
+// recipeRouter.get("/",authenticate, getAllRecipe)
+// recipeRouter.put("/edit",authenticate, authorize([1]), editRecipe)
 module.exports = {
-    recipeRouter,
+    recipeRouter
 }
