1f0ae422cf8fee8f591fb08e8dda6fa5d61a713f
Tran Quan <trannhatquan.2001@gmail.com>
2023-04-16 00:51:42 +0700

    add getRecipeByTitle

--- routers/recipe.routers.js
+++ routers/recipe.routers.js
@@ -3,14 +3,15 @@ const express = require("express");
 const {authenticate} = require("../middlewares/auth/authenticate.js")
 // const {authorize} = require("../middlewares/auth/authorize.js")
 
-const {getInfoRecipe, getFavorite} = require("../controllers/recipe.controllers");
+const {getInfoRecipe, getFavorite, getAllRecipe, getRecipeByTitle} = require("../controllers/recipe.controllers");
 
 const recipeRouter = express.Router();
 //lay day du thong tin mot mon an
 recipeRouter.get("/:idRecipe",authenticate, getInfoRecipe)
 
 recipeRouter.get("/favorite", authenticate, getFavorite) 
-// recipeRouter.get("/",authenticate, getAllRecipe)
+recipeRouter.get("/",authenticate, getAllRecipe)
+recipeRouter.get("/:title",authenticate, getRecipeByTitle)
 // recipeRouter.put("/edit",authenticate, authorize([1]), editRecipe)
 module.exports = {
     recipeRouter
