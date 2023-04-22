ddd40fbe102dc91956118df153d12a3a6ade03b3
Tran Quan <trannhatquan.2001@gmail.com>
2023-04-22 12:11:34 +0700

    fix router

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { userRouter } = require("./user.routers");
 const { accountRouter } = require("./account.routers");
-//const { recipeRourer } = require("./recipe.routers");
+const { recipeRouter } = require("./recipe.routers");
 const { exerciseRouter } = require("./exercise.routers");
 //const { ingredientRouter } = require("./ingredient.routers");
 //const { equipmentRouter } = require("./equipment.routers");
@@ -11,7 +11,7 @@ const rootRouter = express.Router();
 
 rootRouter.use("/user", userRouter);
 rootRouter.use("/account", accountRouter);
-////rootRouter.use("/recipe", recipeRourer);
+rootRouter.use("/recipe", recipeRouter);
 rootRouter.use("/exercise", exerciseRouter);
 //rootRouter.use("/ingredient", ingredientRouter);
 //rootRouter.use("/equipment", equipmentRouter);
