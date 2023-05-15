474232eaa52553e98d5ab678ed98a009cacac7ec
big-bell <minhtri250701@gmail.com>
2023-05-15 11:44:13 +0700

    update excercise

--- controllers/exercise.controllers.js
+++ controllers/exercise.controllers.js
@@ -1,6 +1,3 @@
-// const { raw } = require("body-parser");
-
-// const moment = require('moment'); // require
 const { QueryTypes } = require("sequelize");
 const { Exercise, User_exercise, User, Account } = require("../models");
 const { raw } = require("body-parser");
@@ -9,29 +6,36 @@ const { raw } = require("body-parser");
 // //lay tat thong tin cua 1 bai tap gom cac set, rep, equipment
 
 const getAllexercise = async (req, res) => {
+    const { level } = req.params;
     try {
+        const acc = await Account.findOne({
+            where: { mail: req.mail },
+            include: User
+        })
         // const exercise1 = await Exercise.findAll();
-        const exercise1 = await Exercise.sequelize.query(
-            "select * from exercises",
-            {
-                type: QueryTypes.SELECT,
-                raw: true,
+        const exercise1 = await Exercise.findAll({
+            where: {
+                level: level,
             }
+        }
         );
         res.status(200).json(exercise1);
     } catch (error) {
         res.status(500).json({
-            message: 'Error'
+            message: 'False'
         });
     }
 }
+//Cần sửa lại function này cho phù hợp yêu cầu
 const getDetailexercise = async (req, res) => {
     const { id_exercise } = req.params;
     console.log(id_exercise);
     console.log(Object.values(id_exercise));
     try {
         const details = await Exercise.sequelize.query(
-            `call getDetailexercise(${id_exercise})`,
+            `select e.name, s.index, m.name as menu_name, m.image as menu_image, m.video as menu_video, eq.name as equipment_name
+            from exercises as e, sets as s, menus as m, equipment as eq, menu_equipments as me
+            where e.idExercise = ${id_exercise} and s.idSet = idExercise and s.idSet = m.idMenu and m.idMenu = me.idMenu and me.idMenu = eq.idEquipment;`,
             {
                 // replacements:{}
                 type: QueryTypes.SELECT,
@@ -42,43 +46,11 @@ const getDetailexercise = async (req, res) => {
         res.status(200).json(details);
     } catch (error) {
         res.status(500).json({
-            message: 'Error'
+            message: 'False'
         });
     }
 };
-const selectExercise = async (req, res) => {
-    // const { id_exercise } = req.params;
-    // console.log(id_exercise);
-    // try {
-    //     console.log(req.mail);
-    //     const acc = await Account.findOne({
-    //         where: { mail: req.mail },
-    //         include: User
-    //     })
-
-    //     // const id_user = await User.findOne({
-    //     //     where: { idAcc: acc.idAcc },
-    //     //     include: Account
-    //     // })
-    //     console.log(acc.User.idUser);
-    //     await User_exercise.sequelize.query(
-    //         `call selectExercise(${acc.User.idUser},${id_exercise})`,
-    //         {
-    //             type: QueryTypes.INSERT,
-    //             raw: true,
-    //         }
-
-    //     );
-    //     res.status(200).json({
-    //         message: 'success',
-    //     });
-    // }
-    // catch (error) {
-    //     res.status(500).json({
-    //         message: 'Error'
-    //     });
-    // };
-};
+//Cần viết lại function này
 const userLikeEx = async (req, res) => {
     const { isLike, id_exercise } = req.body;
     try {
@@ -87,68 +59,68 @@ const userLikeEx = async (req, res) => {
             include: User
         })
         console.log(acc.User.idUser, Object.values(isLike), Object.values(id_exercise));
-        // if (Object.values(isLike) == 1) {
-        //     console.log('true')
-        //     await User_exercise.query(
-        //         `update user_exercises set isLike=1 where idExercise='${Object.values(id_exercise)}' and idUser='${acc.User.id_user}'; `,
-        //         {
-        //             type: QueryTypes.UPDATE,
-        //             raw: true,
-        //         });
-        //     res.status(200).json({
-        //         message: 'Sucess'
-        //     });
-        // }
-        // else {
-        //     await User_exercise.query(
-        //         `update user_exercise set sisLike = ${Object.values(isLike)} where idExercise = ${Object.values(id_exercise)} and idUser = ${acc.User.idUser}`,
-        //         {
-        //             type: QueryTypes.UPDATE,
-        //             raw: true,
-        //         }
-        //     )
-        //     res.status(200).json({
-        //         message: 'Sucess'
-        //     });
-        // }
-        await User_exercise.query(
-            `update user_exercises set isLike='${Object.values(isLike)}' where idExercise='${Object.values(id_exercise)}' and idUser='${acc.User.id_user}'; `,
-            {
-                type: QueryTypes.UPDATE,
-                raw: true,
-            });
-        res.status(200).json({
-            message: 'Sucess'
-        });
+
+        if (Object.values(isLike) == 1) (
+            User_history.update({ isLike: 1 }, {
+                where: {
+                    idExercise: Object.values(id_exercise),
+                    idUser: acc.User.idUser
+                }
+            })
+        )
+        else (
+            User_history.update({ isLike: 0 }, {
+                where: {
+                    idExercise: Object.values(id_exercise),
+                    idUser: acc.User.idUser
+                }
+            })
+        )
+
+
     } catch (error) {
         res.status(500).json({
-            message: 'Error'
+            message: 'False'
         });
     }
 };
-
+//Cần sửa lại function này cho phù hợp yêu cầu
 const completeExercise = async (req, res) => {
-    const { id_user, id_exercise } = req.body;
+    const { id_exercise } = req.body;
     try {
-        await User_history.sequelize.query(
-            `call completeExercise(${Object.values(id_user)}, ${Object.value(id_exercise)})`,
-            {
-                type: QueryTypes.UPDATE,
-                raw: true,
+        const acc = await Account.findOne({
+            where: { mail: req.mail },
+            include: User
+        })
+
+        const calo = await Exercise.findOne({ attributes: ['calories'] }, {
+            where: {
+                idExercise: Object.values(id_exercise),
             }
+        }
+        )
+        await User_history.update({ calories_out: calories_out + calo.dataValues() }, {
+            where: {
+                idUser: acc.User.idUser,
+            }
+        }
+
         )
         res.status(200).json({
-            message: 'Sucess'
+            message: 'True'
         });
         // res.status(200).json({
         //     message: 'Sucess'
         // });
     } catch (error) {
         res.status(500).json({
-            message: 'Error'
+            message: 'False'
         })
     }
 };
+const getCmtEx = (req, res) => { }
+
+const getTopEx = (req, res) => { }
 module.exports = {
-    getAllexercise, getDetailexercise, userLikeEx, selectExercise, completeExercise,
+    getAllexercise, getDetailexercise, userLikeEx, completeExercise, getCmtEx, getTopEx
 }
