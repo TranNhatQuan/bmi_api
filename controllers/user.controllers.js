18097c099447f01b16ebeadff2d082c8d720a47c
Tran Quan <trannhatquan.2001@gmail.com>
2023-05-16 22:07:43 +0700

    fix sth

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -135,8 +135,8 @@ const getRecipeHistory = async (req, res) => {
                     idRecipe: randomID,
                     filter: 3,
                 });
-              }
-            
+            }
+
             let breakfast = await Recipe_history.findAll({
                 attributes: ['idRecipe'],
                 where: {
@@ -390,7 +390,7 @@ const getInfoUser = async (req, res) => {
         });
         console.log(acc.User.idUser);
         const user_h = await User.findOne({
-            where:acc.User.idUser,
+            where: acc.User.idUser,
             attributes: ['idUser', 'name', 'gender', 'weight', 'height', 'isShare'],
         })
         res.status(200).json(user_h);
@@ -536,46 +536,59 @@ const editUser = async (req, res) => {
     }
 };
 const getUser = async (req, res) => {
-    const idUser = req.params;
-    const id = idUser['idUser'];
-    console.log(idUser);
+
     try {
+        const { id } = req.params;
+        
         const user = await Userswithbmi.findOne({
             where: {
                 idUser: id,
             },
         });
+
         user_date = await User_history.findOne({
             where: {
                 idUser: id,
             },
-            attributes:['date']
+            attributes: ['date']
         });
-        user.dataValues.date=user_date.dataValues.date;
-        // delete user.dataValues.User_history;
-        const user_recipe = await User_recipe.sequelize.query(
-            `select user_recipes.idRecipe,name,calories,image,cmt,isLike from user_recipes inner join recipes
+        
+        if (user_date !== null) {
+            user.dataValues.date = user_date.dataValues.date;
+
+            let user_recipe = await User_recipe.sequelize.query(
+                `select user_recipes.idRecipe,name,calories,image,cmt,isLike from user_recipes inner join recipes
             on user_recipes.idRecipe= recipes.idRecipe 
             where user_recipes.idUser=${id} and isLike=1`,
-            {
-                type: QueryTypes.SELECT,
-                raw: true,
-            }
-        );
-        const user_ex = await User_exercise.sequelize.query(
-            `select user_exercises.idExercise,name,calories,image,cmt,isLike from user_exercises inner join exercises
+                {
+                    type: QueryTypes.SELECT,
+                    raw: true,
+                }
+            );
+            let user_ex = await User_exercise.sequelize.query(
+                `select user_exercises.idExercise,name,calories,image,cmt,isLike from user_exercises inner join exercises
             on user_exercises.idExercise= exercises.idExercise
             where user_exercises.idUser=${id} and isLike=1`,
-            {
-                type: QueryTypes.SELECT,
-                raw: true,
-            }
-        );
-        res
+                {
+                    type: QueryTypes.SELECT,
+                    raw: true,
+                }
+            );
+            return res
             .status(200)
             .json({
                 user, user_recipe, user_ex
             });
+        }
+        else{
+            return res
+            .status(200)
+            .json({
+                user
+            });
+        }
+
+        
 
     } catch (error) {
         res.status(500).json({
@@ -656,9 +669,9 @@ const editUserHistory = async (req, res) => {
             }
         });
         //console.log(user_his)
-        if(!user_his){
+        if (!user_his) {
             return res.status(404).json({
-                isSuccess:false
+                isSuccess: false
             });
         }
 
@@ -666,14 +679,14 @@ const editUserHistory = async (req, res) => {
         await user_his.save();
 
         res.status(200).json({
-            isSuccess:true,
+            isSuccess: true,
             user_his
         });
 
 
     } catch {
         res.status(500).json({
-            isSuccess:false
+            isSuccess: false
         });
     }
 };
