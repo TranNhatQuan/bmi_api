c09e90ac0bf7231779da871bd49307d2263c0ce0
big-bell <minhtri250701@gmail.com>
2023-05-18 23:35:00 +0700

    Update exercise.controllers.js

--- controllers/exercise.controllers.js
+++ controllers/exercise.controllers.js
@@ -2,15 +2,9 @@ const { QueryTypes } = require("sequelize");
 
 const moment = require('moment'); // require
 
-const { Exercise, User_exercise, User, Account, User_history, sequelize } = require("../models");
-
-const { Exercise, User_exercise, User, Equipment, Account, User_history, Exercise_rank, Menu, Menu_equipment } = require("../models");
+const { Exercise, User_exercise, User, Equipment, Account, User_history, Exercise_rank, Menu, Menu_equipment, sequelize } = require("../models");
 const MySet = require('../models').Set;
 const { raw } = require("body-parser");
-const { Exercise, User_exercise, User, Equipment, Account, User_history, Exercise_rank, Menu, Menu_equipment } = require("../models");
-const MySet = require('../models').Set;
-const { raw } = require("body-parser");
-
 
 // exerciseRouter.get("/", authenticate, getAllexercise);
 // //lay tat thong tin cua 1 bai tap gom cac MySet, rep, equipment
