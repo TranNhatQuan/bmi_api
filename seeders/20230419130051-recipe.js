fb663b3bde4ab4d7b1ee56b742ecc083cf582f8b
viettrile <105573712+viettrile@users.noreply.github.com>
2023-05-22 16:29:44 +0700

    Update 20230419130051-recipe.js

--- seeders/20230419130051-recipe.js
+++ seeders/20230419130051-recipe.js
@@ -35,14 +35,14 @@ module.exports = {
         image: 'https://cdn.tgdd.vn/2021/01/CookProduct/cach-lam-ga-cung4-960x540.jpg',
       },
       {
-        name: 'Boiled Chicken2',
+        name: 'Sauteed chicken breast',
         info: '',
         calories: 400,
         proteins: 40,
         fats: 30,
         carbo: 10,
-        idType: 2,points: 0,
-        image: 'https://cdn.tgdd.vn/2021/01/CookProduct/cach-lam-ga-cung4-960x540.jpg',
+        idType: 1,points: 0,
+        image: 'https://img.tastykitchen.vn/resize/764x-/2021/03/27/ga-xao-mang-tay-f24a.jpg',
       },
       {
         name: 'Boiled Chicken3',
