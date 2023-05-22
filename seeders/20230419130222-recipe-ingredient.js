57fa77574219502ddff9e3e5b06cda0e0f97f500
viettrile <105573712+viettrile@users.noreply.github.com>
2023-05-22 16:35:22 +0700

    Update 20230419130222-recipe-ingredient.js

--- seeders/20230419130222-recipe-ingredient.js
+++ seeders/20230419130222-recipe-ingredient.js
@@ -28,6 +28,12 @@ module.exports = {
         quantity: 2,
         unitName: "g"
       },
+      {
+        idRecipe: 4,
+        idIngredient: 4,
+        quantity: 2,
+        unitName: "g"
+      },
     ]);
   },
 
